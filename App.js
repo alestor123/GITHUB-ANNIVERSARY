64a6c765a2b2849e576aebeec683089f2e9c028c
alestor123 <alestoraldous@gmail.com>
2021-06-29 21:59:56 +0530

    added days left

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 module.exports = async (username, token) => {
   if (!(username && typeof username === 'string' && username.length > 0)) throw new Error('Please enter a vaild username')
   else if (token && typeof token === 'string' && token.length > 0) axios.defaults.headers.common.Authorization = `Bearer ${token}`
+  else throw new Error('Enter a valid token')
   const createdAt = (await axios.get(`https://api.github.com/users/${username}`)).data.created_at
   const pdate = new Date(createdAt)
   const dt = new Date()
@@ -13,7 +14,9 @@ module.exports = async (username, token) => {
     on: ded.toString(),
     date: pdate.toString(),
     pdate: pdate.toISOString().split('T')[0],
-    isAnniversary: dayOfyear(dt) === dayOfyear(pdate)
+    isAnniversary: dayOfyear(dt) === dayOfyear(pdate),
+    diffDate: dayOfyear(pdate) - dayOfyear(dt) 
+
   }
 }
 function dayOfyear (date) {
